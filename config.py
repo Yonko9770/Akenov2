1987b75745c4fc7af146263a9bdfc44f70563277
Kakashi <121147878+Yonko9770@users.noreply.github.com>
2023-03-02 21:32:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,16 +10,16 @@ class Config(object):
 
     API_ID = int(getenv("API_ID", 6))
     API_HASH = getenv("API_HASH", None)
-    ARQ_API_KEY = "PMPTTD-HOMLMF-SRBHNH-RZMWXL-ARQ"
+    ARQ_API_KEY = " VVNCDW-NIBQUU-QKDTOM-JTRWBQ-ARQ"
     STRING_SESSION = getenv("STRING_SESSION", None)
     SPAMWATCH_API = None
     TOKEN = getenv("TOKEN", None)
-    OWNER_ID = int(getenv("OWNER_ID", "5938660179"))  # sᴛᴀʀᴛ @Exon_Robot ᴛʏᴘᴇ /id
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "Abishnoi1M")
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "AbishnoiMF")
-    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001819078701"))
+    OWNER_ID = int(getenv("OWNER_ID", "5700727404"))  # sᴛᴀʀᴛ @Exon_Robot ᴛʏᴘᴇ /id
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "YeahKakashi")
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "Devs_discuissons")
+    LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001809288523"))
     MONGO_URI = getenv("MONGO_DB_URI")
-    REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
+    REDIS_URL = "redis://Kakashi:.Sonu1234@redis-19899.c60.us-west-1-2.ec2.cloud.redislabs.com:19899"
     DATABASE_URL = getenv("DATABASE_URL")
 
     # ɴᴏ ᴇᴅɪᴛ ᴢᴏɴᴇ
