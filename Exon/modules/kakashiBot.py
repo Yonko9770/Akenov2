e75bb4c7868e0f9ad3e2c99e1def73cf54212a59
Kakashi <121147878+Yonko9770@users.noreply.github.com>
2023-03-13 13:48:17 +0530

    Update kakashiBot.py

--- Exon/modules/kakashiBot.py
+++ Exon/modules/kakashiBot.py
@@ -3,18 +3,30 @@ from pyrogram import filters
 from pyrogram.types import message
 from Exon import abishnoi
 
-@abishnoi.on.messege(
-  filters.txt & filters.regex(r"(.*) & -filters.via_bot & -filters.bot,
-                             )
+@pbot.on_message(
+
+    filters.text
+
+    & filters.regex(r"\b[Kk][Aa][Kk][Aa][Ss][Hh][Ii]\b")
+
+    & ~filters.via_bot
+
+    & ~filters.bot,
+
+)
+
 async def upvote(_, message: Message):
-  text = messege.text
-  
-  pattern = re.compile(r"(?:kakashi\s+(\w)", re.IGNORECASE)
-  
-  result = pattern.search(text)
-  
-  if result:
-       word= result.group(1).capitalize()
-  
-       return await messege.reply_text(f"Kakashi! Last Seen a Long Time Ago.")
-  return
+
+    text = message.text
+
+    match = re.search(r"(?<=\b[Kakashi]\b\s)\w+", text)
+
+    if match:
+
+        word = match.group()
+
+        word = word.lower().capitalize()
+
+        return await message.reply_text(f"Hello {word}! I am Dad.")
+
+    return
